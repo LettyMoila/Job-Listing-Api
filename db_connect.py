f56e17e5ba4f4cd962fb8f20285d84cb1bc9edfe
zanele <zanelemanyath@gmail.com>
2023-07-20 23:16:22 +0200

    username to user

--- db_connect.py
+++ db_connect.py
@@ -6,7 +6,7 @@ conn_pool = pool.SimpleConnectionPool(
     database = os.getenv('DB_NAME'),
     host = os.getenv('DB_HOST'),
     port = os.getenv('DB_PORT'),
-    username = os.getenv('DB_USER'),
+    user = os.getenv('DB_USER'),
     password = os.getenv('DB_PASSWORD')
 )
 
