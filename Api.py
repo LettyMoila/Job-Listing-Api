ac84c121000022e26345cece8933cad66a855023
zanele <zanelemanyath@gmail.com>
2023-07-20 20:26:11 +0200

    Final Edit

--- Api.py
+++ Api.py
@@ -12,11 +12,11 @@ CORS(app)
 
 @app.route('/')
 def main():
-    return "Job Listing API"
+    return redirect('http://localhost:3000/')
 
 '''Client Routes and connections'''
 @app.route('/client_profile/<id>', methods=['GET', 'POST'])
-def client_profile_route(id):
+def client_profile_route(id=0):
     client_profile = []
     
     table_instance = db_connect('client_profile')
@@ -59,22 +59,21 @@ def client_profile_route(id):
     else:
         return 'Enter correct data'
 
-#CLIENT EXPERIENCE
-@app.route('/client_profile/<client_id>/experience', methods=['GET', 'POST'])
-def client_experience_route(client_id=0):
+
+
+@app.route('/client_profile/<id>/experience', methods=['GET', 'POST'])
+def client_experience_route(id=0):
     client_exp = []
     
     table_instance = db_connect('client_experience')
     client = db_connect('client_profile')
     
-    
     if request.method == "GET":
         client_id = client.select(condition=f"WHERE id='{id}'")
         rows = table_instance.select()
         for row in rows:
-          
             exp_table = {
-                'client_id': row[client_id],
+                'id': row[client_id],
                 'current_job': row[1],
                 'start_date': row[2],
                 'end_date': row[3],
@@ -95,7 +94,6 @@ def client_experience_route(client_id=0):
         else:
             return 'No information about client experience at the moment'
     elif request.method == "POST":
-        client_id = client.select(condition=f"WHERE id='{id}'")
         current_job = request.form['current_job']
         start_date = request.form['start_date']
         end_date = request.form['end_date']
@@ -104,77 +102,95 @@ def client_experience_route(client_id=0):
         job_location_state = request.form['job_location_state']
         job_location_country = request.form['job_location_country']
         description = request.form['description']
-        table_instance.insert("client_id, current_job, start_date, end_date, company_name, job_location_city, job_location_state, job_location_country, description", 
-                        f"'{client_id}','{current_job}','{start_date}','{end_date}', '{company_name}','{job_location_city}','{job_location_state}','{job_location_country}','{description}'")
+        table_instance.insert("current_job, start_date, end_date, company_name, job_location_city, job_location_state, job_location_country, description", 
+                        f"'{current_job}','{start_date}','{end_date}', '{company_name}','{job_location_city}','{job_location_state}','{job_location_country}','{description}'")
         
-        return 'success'
+        return redirect('http://localhost:3000/jobs')
     else:
         return 'No client experience at the moment'
 
 
-# CLIENT EDUCATION
-@app.route('/client_profile/<id>/education')
+
+@app.route('/client_profile/<id>/education', methods=['GET', 'POST'])
 def client_education_route(id=0):
     client_edu = []
     
     table_instance = db_connect('client_education')
     client = db_connect('client_profile')
     
-    client_id = client.select(condition=f"WHERE id='{id}'")
-    
-    rows = table_instance.select()
-    
-    for row in rows:
-        edu_table = {
-            'client_id': row[client_id],
-            'certificate_name': row[1],
-            'major': row[2],
-            'institute_name': row[3],
-            'start_date': row[4],
-            'end_date': row[5],
-        }
+    if request.method == "GET":
+        client_id = client.select(condition=f"WHERE id='{id}'")
+        rows = table_instance.select()
+        for row in rows:
+            edu_table = {
+                'id': row[client_id],
+                'certificate_name': row[1],
+                'major': row[2],
+                'institute_name': row[3],
+                'start_date': row[4],
+                'end_date': row[5],
+            }
+            
+            client_edu.append(edu_table)
+            
+            edu_dictionary = {
+                'client_education': client_edu
+            }
+            
+            return edu_dictionary
+        else:
+            return 'No information about client education at the moment'
+    elif request.method == "POST":
+        certificate_name = request.form['certificate_name']
+        major = request.form['major']
+        institute_name = request.form['institute_name']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        table_instance.insert("certificate_name, major, institute_name, start_date, end_date", 
+                        f"'{certificate_name}','{major}','{institute_name}', '{start_date}','{end_date}'")
         
-        client_edu.append(edu_table)
-        
-        edu_dictionary = {
-            'client_education': client_edu
-        }
-        
-        return edu_dictionary
+        return redirect('http://localhost:3000/apply/client_skills')
     else:
-        return 'No information about Jclient education at the moment'
-    
-# CLIENT SKILLS
-@app.route('/client_profile/<id>/skills')
+        return 'No client experience at the moment'
+
+
+
+@app.route('/client_profile/<id>/skills', methods=['GET', 'POST'])
 def client_skills(id=0):
-    client_skills = []
+    client_skill = []
     
     table_instance = db_connect('client_skills')
     client = db_connect('client_profile')
-    
-    client_id = client.select(condition=f"WHERE id='{id}'")
-    skills = db_connect('skill_set')
-    skill_id = skills.select(condition=f"WHERE id='{id}'")
-    
     rows = table_instance.select()
     
-    for row in rows:
-        cs_table = {
-            'client_id': row[client_id],
-            'skill_set_id': row[skill_id],
-            'skill_level': row[2]
-        }
+    if request.method == "GET":
+        id = client.select(condition=f"WHERE id='{id}'")
+        for row in rows:
+            cs_table = {
+                'id': row[id],
+                'skill_set': row[1],
+                'skill_level': row[2],
+            }
+            
+            client_skill.append(cs_table)
+            
+            cs_dict = {
+                'client_skills': client_skills
+            }
+            
+            return cs_dict
+        else:
+            return 'No information about client skills at the moment'
+    elif request.method == "POST":
+        skill_level = request.form['skill_level']
+        skill_set = request.form['skill_set']
+        table_instance.insert("skill_level, skill_set",
+                        f"'{skill_level}','{skill_set}'")
         
-        client_skills.append(cs_table)
-        
-        cs_dict = {
-            'client_skills': client_skills
-        }
-        
-        return cs_dict
+        return redirect('http://localhost:3000/apply/thank_you')
     else:
-        return 'No information about client skills at the moment'
- 
+        return 'No client experience at the moment'
+    
  
 '''Job Routes and Connections'''
 @app.route('/job_post/', methods=['GET', 'POST'])
@@ -192,9 +208,10 @@ def job_post_route():
                 'company_id': row[1],
                 'role': row[2],
                 'description': row[3],
-                'location': row[4],
-                'created_at': row[5],
-                'closed_at': row[6]
+                'skill_set': row[4],
+                'location': row[5],
+                'created_at': row[6],
+                'closed_at': row[7]
             }
             
             job_post.append(jp_table)
@@ -209,38 +226,15 @@ def job_post_route():
         role = request.form['tittle']
         location = request.form['location']
         description = request.form['description']
+        skill_set = request.form['skill_set']
         created_at = datetime.now()
         closed_at = datetime.now()
-        table_instance.insert("company_id, role, location, description, created_at, closed_at", f"'{company_id}','{role}','{description}', '{location}','{created_at}','{closed_at}'")
+        table_instance.insert("company_id, role, location, description,skill_set, created_at, closed_at",
+                        f"'{company_id}','{role}','{description}','{skill_set}', '{location}','{created_at}','{closed_at}'")
         
-        return 'success'
+        return redirect('http://localhost:3000/')
 
 
-@app.route('/client_profile/<client_id>/job_post/<job_post_id>/activity')
-def job_post_activity_route(client_id=0, job_post_id=0):
-    jp_activity = []
-    
-    table_instance = db_connect('job_post_activity')
-    
-    rows = table_instance.select()
-    
-    for row in rows:
-        jpa_table = {
-            'client_id': row[0],
-            'job_post_id': row[1],
-            'apply_date': row[2] 
-        }
-        
-        jp_activity.append(jpa_table)
-        
-        jpa_dictionary = {
-            'job_post_activity': jp_activity
-        }
-        
-        return jpa_dictionary
-    else:
-        return 'No information about Job Posts Activity at the moment'
-        
 
 @app.route('/company/', methods=['GET', 'POST'])
 def company_route():
@@ -272,99 +266,60 @@ def company_route():
         website = request.form['website']
         table_instance.insert("name, description, website", f"'{name}','{description}', '{website}'")
         return redirect(url_for('company_route'))
-
-#JOB POST SKILL SET
-@app.route('/job_post/<job_post_id>/skill_set/<skill_set_id>')
-def job_post_skill_set(skill_set_id=0, job_post_id=0):
-    jp_skill_set = []
-    
-    table_instance = db_connect('job_post_skill_set')
-    
-    rows = table_instance.select()
-    
-    for row in rows:
-        jp_skill_set_table = {
-            'skill_set_id': row[0],
-            'job_post_id': row[1],
-            'skill_level': row[2]
-        }
-        
-        jp_skill_set.append(jp_skill_set_table)
-        
-        jp_skill_set_dict = {
-            'job_post_skill_set': jp_skill_set
-        }
-
-        return jp_skill_set_dict
-    else:
-        return 'No information about Job Post Skill Sets at the moment'
-    
-    
-#SKILL SET ROUTE
-@app.route('/skill_set/', methods=['GET', 'POST']) 
-def skill_set():
-    skill_set = []
-    
-    table_instance = db_connect('skill_set')
-    
-    rows = table_instance.select()
-    
-    for row in rows:
-        skill_set_table = {
-            'id': row[0],
-            'skill_set_name': row[1]
-        }
-        
-        skill_set.append(skill_set_table)
-        
-        skill_set_dict = {
-            'skill_set': skill_set
-        }    
-        return skill_set_dict
-    else:
-        return 'No information about Skill Sets at the moment'
+ 
     
 #JOB LOCATION ROUTE 
-@app.route('/job_location')
+@app.route('/job_location', methods=['GET'])
 def location_route():
     temp_location = []
     
     location_instance = db_connect('job_location')
     
     rows = location_instance.select()
+    jobpost_instance = db_connect('job_post')
     
-    for row in rows:
-        location = {
-            'id': row[0],
-            'name': row[1],
-            'description': row[2],
-            'website': row[3]
-        }
-        
-        temp_location.append(location)
-        
-        location_dict = {
-            'job_location': temp_location
-        }
-        
-        return location_dict
-    else:
-        return 'No information about job location at the moment'
+    jp_id = jobpost_instance.select(condition=f"WHERE id='{id}'")
     
+    if request.method == "GET":
+        for row in rows:
+            location = {
+                'id': row[0],
+                'street_add': row[1],
+                'city': row[2],
+                'province': row[3],
+                'country': row[4],
+                'code': row[5],
+                'job_id': row[jp_id],
+            }
+            
+            temp_location.append(location)
+            
+            location_dict = {
+                'job_location': temp_location
+            }
+            
+            return location_dict
+        else:
+            return 'No information about job location at the moment'
+
+  
 #JOB TYPE ROUTE 
-@app.route('/job_type')
+@app.route('/job_type', methods=['GET'])
 def job_type_route():
     jtype = []
     
     job_type_instance = db_connect('job_type')
     
     rows = job_type_instance.select()
+    jobpost_instance = db_connect('job_post')
+    
+    jp_id = jobpost_instance.select(condition=f"WHERE id='{id}'")
     
     for row in rows:
         typeJob = {
             "id" : row[0],
             "job_type ": row[1],
-            "job_id": row[2]
+            "job_id": row[jp_id]
         }
         
         jtype.append(typeJob)
